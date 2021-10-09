9bb70e6b5f3a04f86b9672c257c6bca75b1ceb44
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-09 14:39:46 -0500

    Add: buttons into the search page

--- pages/search.js
+++ pages/search.js
@@ -6,11 +6,15 @@ export default function Search() {
     <div>
       <Header />
       <main className='flex'>
-        <section>
+        <section className='flex-grow pt-14 px-16'>
           <p className='text-xs'>300+ Stays for 5 number of guests</p>
           <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
-          <div className='hidden lg:inline-flex'>
+          <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
             <p className='btn-default'>Cancellation Flexibility</p>
+            <p className='btn-default'>Type of place</p>
+            <p className='btn-default'>Price</p>
+            <p className='btn-default'>Rooms & Beds</p>
+            <p className='btn-default'>More Filters</p>
           </div>
         </section>
       </main>
