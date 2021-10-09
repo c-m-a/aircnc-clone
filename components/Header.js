42de24f6bd208008b4e05fc56547bed0673e7dcd
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-08 21:41:36 -0500

    Add: search box in the Header component

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
-import Image from 'next/image';
+import Image from 'next/image'
+import { SearchIcon } from '@heroicons/react/solid'
 
 export default function Header() {
   return (
@@ -11,7 +12,16 @@ export default function Header() {
           objectPosition='left'
         />
       </div>
-      <div></div>
+      <div className='flex items-center md:border-2 md:shadow-sm rounded-full py-2'>
+        <input
+          className='flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400'
+          type='text'
+          placeholder='Start your search'
+        />
+        <SearchIcon
+          className='h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer hidden md:inline-flex md:mx-2'
+        />
+      </div>
       <div></div>
     </header>
   )
