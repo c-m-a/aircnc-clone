88e9a1faf49bffd11f3b3d44646651c132bdbd61
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-09 16:19:15 -0500

    Add: InfoCard component to search page

--- pages/search.js
+++ pages/search.js
@@ -3,8 +3,9 @@ import { format } from 'date-fns'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import InfoCard from '../components/InfoCard'
 
-export default function Search() {
+export default function Search({ searchResults }) {
   const fmtDate = 'dd MMM yy'
   const router = useRouter()
   const { location, startDate, endDate, noOfGuests } = router.query
@@ -26,9 +27,35 @@ export default function Search() {
             <p className='btn-default'>Rooms & Beds</p>
             <p className='btn-default'>More Filters</p>
           </div>
+
+          <div className='flex flex-col'>
+            { searchResults.map(({ img, location, title, description, star, price, total }, idx) => (
+                <InfoCard
+                  key={idx}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+            )) }
+          </div>
         </section>
       </main>
       <Footer />
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const searchResults = await fetch('https://links.papareact.com/isz')
+    .then(res => res.json())
+
+  return {
+    props: {
+      searchResults
+    }
+  }
 }
