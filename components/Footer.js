754383bce264fd269a6ddfae7066174a4d418854
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-09 12:41:06 -0500

    Add: Footer component

---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+export default function Footer() {
+  return (
+    <footer className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+      <div className='space-y-4 text-sm text-gray-800'>
+        <h5 className='font-bold'>ABOUT</h5>
+        <p>How Aircnc works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Aircnc Plus</p>
+        <p>Aircnc Luxe</p>
+      </div>
+
+      <div className='space-y-4 text-sm text-gray-800'>
+        <h5 className='font-bold'>COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>This is pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Cma Fam</p>
+      </div>
+
+      <div className='space-y-4 text-sm text-gray-800'>
+        <h5 className='font-bold'>HOST</h5>
+        <p>Cma React</p>
+        <p>Presents</p>
+        <p>React Developer</p>
+        <p>100k Salary</p>
+        <p>Join Now</p>
+      </div>
+
+      <div className='space-y-4 text-sm text-gray-800'>
+        <h5 className='font-bold'>SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>On Youtube</p>
+        <p>Easter Eggs</p>
+        <p>For the Win</p>
+      </div>
+    </footer>
+  )
+}
+
