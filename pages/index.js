e2bd211d8969193038a00976d68a2821bed54b1c
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-09 08:15:19 -0500

    Add: grid styles to Nearby places

--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,16 @@ export default function Home({ exploreData }) {
       <main className='max-w-7xl mx-auto px-8 sm:px-16'>
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
-          { exploreData?.map(({ img, location, distance }, idx) => (
-            <SmallCard
-              key={idx}
-              img={img}
-              location={location}
-              distance={distance}
-            />
-          )) }
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            { exploreData?.map(({ img, location, distance }, idx) => (
+                <SmallCard
+                  key={idx}
+                  img={img}
+                  location={location}
+                  distance={distance}
+                />
+            )) }
+          </div>
         </section>
       </main>
     </div>
