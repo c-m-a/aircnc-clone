f7076f2dec5fcaab075b418904da924fb1978373
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-10 06:47:44 -0500

    Add: constant MAP_STYLE and MAPBOX_KEY to environment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   images: {
     domains: ['links.papareact.com'],
+  },
+  env: {
+    MAPBOX_KEY: 'pk.eyJ1IjoiYW5pY21hIiwiYSI6ImNpaHdhYnh4bTAyajJ2NGtva2NjdGNkbWEifQ.uMT96rj47Cr8gy5NTb_J-g',
+    MAP_STYLE: 'mapbox://styles/anicma/ckukp60qj8cfu19nyh1g9yst7'
   }
 }
