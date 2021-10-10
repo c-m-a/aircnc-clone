417d4ae582c2c876ae1440324becdae6120533e4
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-10 06:45:29 -0500

    Add: React map GL and logic to show the map

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,23 @@
-export default function Map() {
-  return (
-    <div>
+import { useState } from 'react'
+import ReactMapGL from 'react-map-gl'
 
-    </div>
+export default function Map() {
+  const [viewport, setViewport] = useState({
+    width: '100%',
+    height: '100%',
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 11
+  })
+
+  return (
+    <ReactMapGL
+      mapStyle={process.env.MAP_STYLE}
+      mapboxApiAccessToken={process.env.MAPBOX_KEY}
+      {...viewport}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    >
+    </ReactMapGL>
   )
 }
 
