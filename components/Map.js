d90f7993885abd6a2795844e567417a76f679aa5
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-10 10:09:18 -0500

    Add: Geolib and logic to show Marker and Popup

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,25 @@
 import { useState } from 'react'
-import ReactMapGL from 'react-map-gl'
+import Image from 'next/image'
 
-export default function Map() {
+import { StarIcon } from '@heroicons/react/solid'
+import getCenter from 'geolib/es/getCenter'
+import ReactMapGL, { Marker, Popup } from 'react-map-gl'
+
+export default function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+  // Transform the search obj into the { latitude: ..., longitude: ... }
+  const coordinates = searchResults.map(r => ({
+    longitude: r.long,
+    latitude: r.lat
+  }))
+
+  const center = getCenter(coordinates)
+
   const [viewport, setViewport] = useState({
     width: '100%',
     height: '100%',
-    latitude: 37.7577,
-    longitude: -122.4376,
+    latitude: center.latitude,
+    longitude: center.longitude,
     zoom: 11
   })
 
@@ -17,6 +30,52 @@ export default function Map() {
       {...viewport}
       onViewportChange={nextViewport => setViewport(nextViewport)}
     >
+      { searchResults.map((r, idx) => (
+        <div
+          key={idx}
+        >
+          <Marker
+            className='rounded-full border font-semibold bg-white px-3 py-1 hover:z-50 hover:text-lg transition duration-100 ease-out'
+            longitude={r.long}
+            latitude={r.lat}
+            offsetLeft={-20}
+            offsetTop={-20}
+          >
+            <p
+              role='img'
+              onClick={() => setSelectedLocation(r)}
+              aria-label='price-tag'
+            >{r.price}</p>
+          </Marker>
+
+          { selectedLocation.long === r.long ? (
+            <Popup
+              className='rounded-4xl z-50 max-w-[21.2rem]'
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={r.lat + .0013}
+              longitude={r.long}
+            >
+              <div className='relative h-24 w-full md:h-52 md:w-80 flex-shrink-0 mb-5'>
+                <Image
+                  className='border-b'
+                  src={r.img}
+                  layout='fill'
+                  objectFit='cover'
+                />
+              </div>
+              <div className='px-3'>
+                <p className='flex items-center'>
+                  <StarIcon className='h-5 text-red-400 mr-1' />
+                  {r.star}
+                </p>
+                <p className='text-lg whitespace-pre-line'>{r.title}</p>
+                <p className='text-lg lg:text-2xlg font-semibold pb-2'>{r.price}</p>
+              </div>
+            </Popup>
+          ) : (null)}
+        </div>
+      )) }
     </ReactMapGL>
   )
 }
