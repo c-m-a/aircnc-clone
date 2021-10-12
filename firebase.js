9bc0d69befbf9e52f6aa677082fc0f128504b53f
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-12 10:45:49 -0500

    Refactor: changing the way to import firebase ver. 9

--- firebase.js
+++ firebase.js
@@ -1,22 +1,21 @@
-import firebase from 'firebase'
-import 'firebase/storage'
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoYz7X8SVpgrBHl-JjFoxQgutJGRWI3fQ",
-  authDomain: "fansbox-41e3b.firebaseapp.com",
-  databaseURL: "https://fansbox-41e3b.firebaseio.com",
-  projectId: "fansbox-41e3b",
-  storageBucket: "fansbox-41e3b.appspot.com",
-  messagingSenderId: "411234661108",
-  appId: "1:411234661108:web:33f0c9a404a9acfa3424a1",
-  measurementId: "G-C50REPDPX7"
+  apiKey: 'AIzaSyBoYz7X8SVpgrBHl-JjFoxQgutJGRWI3fQ',
+  authDomain: 'fansbox-41e3b.firebaseapp.com',
+  databaseURL: 'https://fansbox-41e3b.firebaseio.com',
+  projectId: 'fansbox-41e3b',
+  storageBucket: 'fansbox-41e3b.appspot.com',
+  messagingSenderId: '411234661108',
+  appId: '1:411234661108:web:33f0c9a404a9acfa3424a1',
+  measurementId: 'G-C50REPDPX7'
 }
 
-const app = !firebase.apps.length ?
-  firebase.initializeApp(firebaseConfig) : firebase.app()
-
-const db = app.firestore()
-const storage = firebase.storage()
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const storage = getFirestore(app)
 
 export { db, storage }
+export default app;
 
