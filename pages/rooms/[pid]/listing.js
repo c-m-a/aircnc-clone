e7e88df726a70c92503cc20c1d4f344d73d9784f
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-12 18:35:17 -0500

    Add: states, logic and EditRoomOpts component

--- pages/rooms/[pid]/listing.js
+++ pages/rooms/[pid]/listing.js
@@ -1,5 +1,39 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/dist/client/router'
+
+import firebase, { db } from '../../../firebase'
+import { doc, getDoc } from 'firebase/firestore'
+
+import Footer from '../../../components/Footer'
+import Header from '../../../components/Header'
+import EditRoomOpts from '../../../components/EditRoomOpts'
+import SideBar from '../../../components/SideBar'
+
 export default function Listing() {
+  const router = useRouter()
+  const roomId = router.query.pid
+  const [showModal, setShowModal] = useState(false)
+  const [room, setRoom] = useState({})
+
+  useEffect(async () => {
+    if (roomId === undefined) return false;
+
+    const roomRef = doc(db, 'rooms', roomId)
+    const roomSnap = await getDoc(roomRef)
+    setRoom(roomSnap.data())
+  }, [roomId])
+
   return (
-    <div>Listing</div>
+    <>
+      <Header setShowModal={setShowModal} />
+      <main className='my-5'>
+        <SideBar />
+        <EditRoomOpts
+          room={room}
+          setRoom={setRoom}
+        />
+      </main>
+      <Footer />
+    </>
   )
 }
