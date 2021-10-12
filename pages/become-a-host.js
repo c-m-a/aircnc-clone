614f0725104a72549c41c4e3c1a89243921b1ea0
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-12 18:32:52 -0500

    Delete: space in pages/become-a-host.js

--- pages/become-a-host.js
+++ pages/become-a-host.js
@@ -5,7 +5,6 @@ export default function BecomeAHost() {
     <div>
       <section className='flex-grow pt-14 px-16'>
         <h1 className='text-3xl font-semibold mt-2 mb-6'>Start a new listing</h1>
-
         <RoomOpts />
       </section>
     </div>
