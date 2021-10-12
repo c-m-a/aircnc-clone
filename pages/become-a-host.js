dfc114a43fbd96fc209573c438e9341742fdd436
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-12 18:57:22 -0500

    Add: Put Header y Footer components to become-a-host.js

--- pages/become-a-host.js
+++ pages/become-a-host.js
@@ -1,13 +1,23 @@
+import { useState } from 'react'
+
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 import RoomOpts from '../components/RoomOpts'
 
 export default function BecomeAHost() {
+  const [showModal, setShowModal] = useState(false)
+
   return (
-    <div>
-      <section className='flex-grow pt-14 px-16'>
-        <h1 className='text-3xl font-semibold mt-2 mb-6'>Start a new listing</h1>
-        <RoomOpts />
-      </section>
-    </div>
+    <>
+      <Header setShowModal={setShowModal} />
+      <main className='py-14 px-16'>
+        <section>
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>Start a new listing</h1>
+          <RoomOpts />
+        </section>
+      </main>
+      <Footer />
+    </>
   )
 }
 
