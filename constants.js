e233d9fb0e4e70434e069f660fb14ba2e596747f
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-12 07:28:57 -0500

    Add: constants.js file

---
+++ constants.js
@@ -0,0 +1,51 @@
+export const
+  APP_NAME = 'Aircnc',
+  ROOM_CFG = {
+    homeType: [
+      { id: 0, name: 'Home Type' },
+      { id: 1, name: 'Apartment' },
+      { id: 2, name: 'House' },
+      { id: 3, name: 'Hotel Room' },
+    ],
+    roomType: [
+      { id: 0, name: 'Room Type' },
+      { id: 1, name: 'Entire' },
+      { id: 2, name: 'Private' },
+      { id: 3, name: 'Shared' },
+    ],
+    accommodate: [
+      { id: 0, name: 'Accommodate' },
+      { id: 1, name: '1 person' },
+      { id: 2, name: '2 people' },
+      { id: 3, name: '3 people' },
+      { id: 4, name: '4 people+' },
+    ],
+    bedrooms: [
+      { id: 0, name: 'Bedrooms' },
+      { id: 1, name: '1 bedroom' },
+      { id: 2, name: '2 bedrooms' },
+      { id: 3, name: '3 bedrooms' },
+    ],
+    bathrooms: [
+      { id: 0, name: 'Bathrooms' },
+      { id: 1, name: '1 bathroom' },
+      { id: 2, name: '2 bathrooms' },
+      { id: 3, name: '3 bathrooms' },
+    ],
+  },
+  FOOTER = [
+    {
+      title: 'About',
+      submenus: [
+        {
+          name: 'How Airbnb works',
+          link: '/'
+          },
+        {
+          name: 'Newsroom',
+          link: '/'
+        },
+      ]
+    }
+  ]
+
