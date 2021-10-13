bb5d47bd098bc2d01e2e04dbe2ddbaff686e2bb3
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-13 08:26:31 -0500

    Add: FORM constants, changing to Absolute Module path aliases

--- pages/rooms/[pid]/listing.js
+++ pages/rooms/[pid]/listing.js
@@ -4,15 +4,26 @@ import { useRouter } from 'next/dist/client/router'
 import firebase, { db } from '../../../firebase'
 import { doc, getDoc } from 'firebase/firestore'
 
-import Footer from '../../../components/Footer'
-import Header from '../../../components/Header'
-import EditRoomOpts from '../../../components/EditRoomOpts'
-import SideBar from '../../../components/SideBar'
+import Footer from '@components/Footer'
+import Header from '@components/Header'
+import EditRoomOpts from '@components/EditRoomOpts'
+import SideBar from '@components/SideBar'
+
+import {
+  FORM_OPTS,
+  FORM_PRICING,
+  FORM_DESCRIPTION,
+  FORM_PHOTOS,
+  FORM_AMENITIES,
+  FORM_LOCATION
+} from '@root/constants'
+
 
 export default function Listing() {
   const router = useRouter()
   const roomId = router.query.pid
   const [showModal, setShowModal] = useState(false)
+  const [form, setForm] = useState(FORM_OPTS)
   const [room, setRoom] = useState({})
 
   useEffect(async () => {
@@ -26,12 +37,22 @@ export default function Listing() {
   return (
     <>
       <Header setShowModal={setShowModal} />
-      <main className='my-5'>
-        <SideBar />
-        <EditRoomOpts
-          room={room}
-          setRoom={setRoom}
-        />
+      <main className='my-5 py-14 px-16'>
+        <h1 className='text-3xl font-semibold mt-2 mb-6'>Your listing</h1>
+        <section className='flex'>
+          <SideBar
+            room={room}
+            setRoom={setRoom}
+            setForm={setForm}
+          />
+          { form === FORM_OPTS &&
+            <EditRoomOpts
+              room={room}
+              setRoom={setRoom}
+              setForm={setForm}
+            />
+          }
+        </section>
       </main>
       <Footer />
     </>
