c7c14c35ba44b271b92f72df277d4a049648b21e
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-13 19:41:48 -0500

    Add: mapbox and facebook keys, images domains

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   images: {
-    domains: ['links.papareact.com'],
+    domains: [
+      'links.papareact.com',
+      'firebasestorage.googleapis.com',
+      'platform-lookaside.fbsbx.com'
+    ],
   },
   env: {
     MAPBOX_KEY: 'pk.eyJ1IjoiYW5pY21hIiwiYSI6ImNpaHdhYnh4bTAyajJ2NGtva2NjdGNkbWEifQ.uMT96rj47Cr8gy5NTb_J-g',
-    MAP_STYLE: 'mapbox://styles/anicma/ckukp60qj8cfu19nyh1g9yst7'
-  }
+    MAP_STYLE: 'mapbox://styles/anicma/ckukp60qj8cfu19nyh1g9yst7',
+    FACEBOOK_CLIENT_ID: '589262328783696',
+    FACEBOOK_CLIENT_SECRET: '92c501df7729ed936fe9f994c3034287',
+  },
 }
