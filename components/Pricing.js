0c3ec718e40d47ef34b9748208caa8078221b4d5
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-14 06:30:43 -0500

    Add: price input field and button save into Pricing component

--- components/Pricing.js
+++ components/Pricing.js
@@ -1,6 +1,26 @@
 export default function Pricing() {
   return (
-    <div>Pricing</div>
+    <div className='mt-5 p-2 w-full'>
+      <div className='flex mb-5'>
+        <label
+          className='font-medium text-lg px-4 my-auto'
+          htmlFor='price'>Price</label>
+        <input
+          className='border p-2 w-full'
+          id='price'
+          name='price'
+          type='number'
+          placeholder='eg: 100'
+        />
+      </div>
+      <div className='flex justify-center w-full'>
+        <button
+          className='btn-primary w-80'
+        >
+          Save
+        </button>
+      </div>
+    </div>
   )
 }
 
