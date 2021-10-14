d1c2b6ee6c70ff6ac89d38122ba33a2ef117dae2
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-14 06:05:00 -0500

    Add: setForm and formOpt props to SideBarRow component

--- components/SideBarRow.js
+++ components/SideBarRow.js
@@ -1,6 +1,9 @@
-export default function SideBarRow({ title }) {
+export default function SideBarRow({ title, setForm, formOpt }) {
   return (
-    <div className='cursor-pointer py-3'>
+    <div
+      className='cursor-pointer py-3'
+      onClick={() => setForm(formOpt)}
+    >
       <p className='text-lg font-medium'>{title}</p>
     </div>
   )
