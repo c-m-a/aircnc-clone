f03f4bcf240e74f782f986130c12cc694901771d
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-14 21:55:06 -0500

    Add: Hero Icons and Icon prop to set to every option

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,4 +1,11 @@
-import SideBarRow from './SideBarRow'
+import {
+  CollectionIcon,
+  CurrencyDollarIcon,
+  DocumentTextIcon,
+  PhotographIcon,
+  WifiIcon,
+  LocationMarkerIcon
+} from '@heroicons/react/outline'
 
 import {
   FORM_LISTING,
@@ -9,42 +16,50 @@ import {
   FORM_LOCATION
 } from '@root/constants'
 
+import SideBarRow from './SideBarRow'
+
 
 export default function SideBar({ form, setForm }) {
   return (
-    <section className='p-2 min-w-[20rem] max-w-[30rem]'>
+    <section className='p-2 min-w-[3.8rem] max-w-[30rem] w-80'>
       <SideBarRow
         title='Listing'
+        Icon={CollectionIcon}
         setForm={setForm}
         formOpt={FORM_LISTING}
         active={form === FORM_LISTING}
       />
       <SideBarRow
         title='Pricing'
+        Icon={CurrencyDollarIcon}
         setForm={setForm}
         formOpt={FORM_PRICING}
         active={form === FORM_PRICING}
       />
       <SideBarRow
         title='Description'
+        Icon={DocumentTextIcon}
         setForm={setForm}
         formOpt={FORM_DESCRIPTION}
         active={form === FORM_DESCRIPTION}
       />
       <SideBarRow
         title='Photos'
+        Icon={PhotographIcon}
         setForm={setForm}
         formOpt={FORM_PHOTOS}
         active={form === FORM_PHOTOS}
       />
       <SideBarRow
         title='Amenities'
+        Icon={WifiIcon}
         setForm={setForm}
         formOpt={FORM_AMENITIES}
         active={form === FORM_AMENITIES}
       />
       <SideBarRow
         title='Location'
+        Icon={LocationMarkerIcon}
         setForm={setForm}
         formOpt={FORM_LOCATION}
         active={form === FORM_LOCATION}
--- components/SideBarRow.js
+++ components/SideBarRow.js
@@ -1,10 +1,16 @@
-export default function SideBarRow({ title, setForm, formOpt, active }) {
+export default function SideBarRow({ Icon, title, setForm, formOpt, active }) {
   return (
     <div
       className={ active ? 'side-bar-btn bg-gray-100' : 'side-bar-btn' }
       onClick={() => setForm(formOpt)}
     >
-      <p className='text-lg font-medium'>{title}</p>
+      <div className='flex items-center space-x-3'>
+        { Icon && (
+          <Icon className='h-5 w-5' />
+        ) }
+        <p className='text-lg font-medium hidden sm:inline-flex'>{title}</p>
+      </div>
+      <i></i>
     </div>
   )
 }
