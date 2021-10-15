8a3929a11ecc528b8d38ff2d3bfb8df6c2249fbf
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-14 19:53:06 -0500

    Add: html elements to Description

--- components/Description.js
+++ components/Description.js
@@ -1,6 +1,31 @@
 export default function Description() {
   return (
-    <div>Description</div>
+    <div className='mt-5 p-2 w-full'>
+      <div className='mb-4'>
+        <input
+          className='border-2 border-gray-500 p-2 rounded-lg w-full'
+          type='text'
+          name='listing_name'
+          placeholder='what is your listing name?'
+        />
+      </div>
+      <div className='mb-5 p-2 border-2 border-gray-500 rounded-lg'>
+        <textarea
+          className='w-full outline-none'
+          id='summary'
+          name='summary'
+          rows='10'
+        >
+        </textarea>
+      </div>
+      <div className='flex justify-center w-full'>
+        <button
+          className='btn-primary w-80'
+        >
+          Save
+        </button>
+      </div>
+    </div>
   )
 }
 
