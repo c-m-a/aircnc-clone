87a59af35b6fd6aad50dd9a94b12d85783bda759
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-15 07:39:44 -0500

    Add: input element into Location

--- components/Location.js
+++ components/Location.js
@@ -1,6 +1,19 @@
 export default function Location() {
   return (
-    <div>Location</div>
+    <div className='mt-5 p-2 w-full'>
+      <div className='mb-5 w-full'>
+        <label
+          className='font-medium text-lg px-4 my-auto'
+          htmlFor='price'>Location</label>
+        <input
+          className='border-2 border-gray-500 p-2 rounded-lg w-full'
+          id='location'
+          name='location'
+          type='text'
+          placeholder='Listing address...'
+        />
+      </div>
+    </div>
   )
 }
 
