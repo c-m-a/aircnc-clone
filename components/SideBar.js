c223e3b3e9604e1cc64c621b850b2d3fe03260da
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-14 21:09:45 -0500

    Add: send active prop to SideBarRow to change the button style

--- components/SideBar.js
+++ components/SideBar.js
@@ -10,38 +10,44 @@ import {
 } from '@root/constants'
 
 
-export default function SideBar({ setForm }) {
+export default function SideBar({ form, setForm }) {
   return (
     <section className='p-2 min-w-[20rem] max-w-[30rem]'>
       <SideBarRow
         title='Listing'
         setForm={setForm}
         formOpt={FORM_LISTING}
+        active={form === FORM_LISTING}
       />
       <SideBarRow
         title='Pricing'
         setForm={setForm}
         formOpt={FORM_PRICING}
+        active={form === FORM_PRICING}
       />
       <SideBarRow
         title='Description'
         setForm={setForm}
         formOpt={FORM_DESCRIPTION}
+        active={form === FORM_DESCRIPTION}
       />
       <SideBarRow
         title='Photos'
         setForm={setForm}
         formOpt={FORM_PHOTOS}
+        active={form === FORM_PHOTOS}
       />
       <SideBarRow
         title='Amenities'
         setForm={setForm}
         formOpt={FORM_AMENITIES}
+        active={form === FORM_AMENITIES}
       />
       <SideBarRow
         title='Location'
         setForm={setForm}
         formOpt={FORM_LOCATION}
+        active={form === FORM_LOCATION}
       />
     </section>
   )
--- components/SideBarRow.js
+++ components/SideBarRow.js
@@ -1,7 +1,7 @@
 export default function SideBarRow({ title, setForm, formOpt, active }) {
   return (
     <div
-      className='cursor-pointer p-3 hover:bg-gray-100 rounded-lg'
+      className={ active ? 'side-bar-btn bg-gray-100' : 'side-bar-btn' }
       onClick={() => setForm(formOpt)}
     >
       <p className='text-lg font-medium'>{title}</p>
