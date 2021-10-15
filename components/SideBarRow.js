d11c3b01585f165c09c92f86e79810143cb29211
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-14 20:06:36 -0500

    Add: formOpt and active props into SideBarRow

--- components/SideBarRow.js
+++ components/SideBarRow.js
@@ -1,7 +1,7 @@
-export default function SideBarRow({ title, setForm, formOpt }) {
+export default function SideBarRow({ title, setForm, formOpt, active }) {
   return (
     <div
-      className='cursor-pointer py-3'
+      className='cursor-pointer p-3 hover:bg-gray-100 rounded-lg'
       onClick={() => setForm(formOpt)}
     >
       <p className='text-lg font-medium'>{title}</p>
