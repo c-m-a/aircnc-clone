b91c9b35db01353278cccf91e02fae23c1fa1229
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-15 07:33:53 -0500

    Delete: button Save from Pricing

--- components/Pricing.js
+++ components/Pricing.js
@@ -1,7 +1,7 @@
 export default function Pricing() {
   return (
     <div className='mt-5 p-2 w-full'>
-      <div className='flex mb-5'>
+      <div className='mb-5 w-full'>
         <label
           className='font-medium text-lg px-4 my-auto'
           htmlFor='price'>Price</label>
@@ -12,13 +12,6 @@ export default function Pricing() {
           type='number'
           placeholder='eg: 100'
         />
-      </div>
-      <div className='flex justify-center w-full'>
-        <button
-          className='btn-primary w-80'
-        >
-          Save
-        </button>
       </div>
     </div>
   )
