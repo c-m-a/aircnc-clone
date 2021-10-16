e02460816412cc87d3d9051513babde0ea1e3446
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-10-16 06:58:40 -0500

    Delete: main tag and elements inside to move to Login component

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,7 @@ import { useState } from 'react'
 import Banner from '@components/Banner'
 import Footer from '@components/Footer'
 import Header from '@components/Header'
-import LargeCard from '@components/LargeCard'
-import Login from '@components/Login'
-import MediumCard from '@components/MediumCard'
-import SmallCard from '@components/SmallCard'
+import Main from '@components/Main'
 
 export default function Home({ exploreData, cardsData, session }) {
   return (
@@ -21,41 +18,7 @@ export default function Home({ exploreData, cardsData, session }) {
 
       <Header />
       <Banner />
-
-      <main className='max-w-7xl mx-auto'>
-        <section className='pt-6'>
-          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            { exploreData?.map(({ img, location, distance }, idx) => (
-                <SmallCard
-                  key={idx}
-                  img={img}
-                  location={location}
-                  distance={distance}
-                />
-            )) }
-          </div>
-        </section>
-        <section>
-          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
-          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
-            { cardsData?.map(({ img, title }, idx) => (
-              <MediumCard
-                key={idx}
-                img={img}
-                title={title}
-              />
-            )) }
-          </div>
-        </section>
-
-        <LargeCard
-          img='https://links.papareact.com/4cj'
-          title='The Greatest Outdoors'
-          description='Wishlist curated by Aircnc'
-          buttonText='Get inspired!'
-        />
-      </main>
+      <Main exploreData={exploreData} cardsData={cardsData} />
       <Footer />
     </div>
   )
